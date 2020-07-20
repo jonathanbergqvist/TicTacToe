72b597f10c4354c8c10b5ef5db86e50e964a8db7
Jonathan <jonathan.bergqvist@hotmail.com>
2020-07-20 02:38:36 +0200

    Fix to enable play of game.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,6 +12,9 @@ public class Main {
     static ArrayList<Character> column_2 = new ArrayList<>();
     static ArrayList<Character> diagonal_tlbr = new ArrayList<>(); // Top left to bottom right
     static ArrayList<Character> diagonal_trbl = new ArrayList<>(); // Top right to bottom left
+    static ArrayList<Character> xPlayerReference = new ArrayList<>();
+    static ArrayList<Character> oPlayerReference = new ArrayList<>();
+    final static Character emptySlot = ' ';
     static int selected_x_cord;
     static int selected_y_cord;
     static boolean gameComplete = false;
@@ -21,12 +23,11 @@ public class Main {
     // TODO: Check if the user input results in game won.
     public static void main(String[] args) {
         playArea = createEmptyBoard();
-        while (!gameComplete) {
-            //System.out.println("BOARD: " + playArea);
+        while (gameComplete == false) {
             for (ArrayList<Character> playAreaRow : playArea) {
-                //System.out.println(playAreaRow);
+                System.out.println(playAreaRow);
             }
-
+            checkIfVictory();
             turnOfGame();
             roundCounter++;
         }
@@ -34,13 +35,17 @@ public class Main {
     }
 
     public static ArrayList<ArrayList<Character>> createEmptyBoard(){
-        ArrayList<Character> emptyRow = new ArrayList<Character>();
         for (int i = 0; i < 3; i++) {
-            emptyRow.add(null);
+            row_0.add(emptySlot);
+            row_1.add(emptySlot);
+            row_2.add(emptySlot);
+            xPlayerReference.add('X');
+            oPlayerReference.add('O');
         }
-        for (int i = 0; i < 3; i++) {
-            playArea.add(emptyRow);
-        }
+        playArea.add(row_0);
+        playArea.add(row_1);
+        playArea.add(row_2);
+
         return playArea;
 
     }
@@ -55,7 +60,7 @@ public class Main {
         row_2 = playArea.get(2);
 
         for (ArrayList<Character> row : playArea) {
-            System.out.println("ROW: " + row);
+            //System.out.println("ROW: " + row);
             // Columns
             // Create initial columns
             //System.out.println("COL: " + column_0.size());
@@ -84,12 +89,43 @@ public class Main {
         }
     }
 
+    public static void checkIfVictory() {
+
+        System.out.println("R0: " + row_0);
+        System.out.println("R1: " + row_1);
+        System.out.println("R2: " + row_2);
+        System.out.println("C0: " + column_0);
+        System.out.println("C1: " + column_1);
+        System.out.println("C2: " + column_2);
+        System.out.println("D0: " + diagonal_trbl);
+        System.out.println("D1: " + diagonal_trbl);
+
+        // TODO: Fix columns and diagonals.
+        if (row_0.equals(xPlayerReference) || row_1.equals(xPlayerReference) || row_2.equals(xPlayerReference) ||
+                column_0.equals(xPlayerReference) || column_1.equals(xPlayerReference) || column_2.equals(xPlayerReference) ||
+                diagonal_tlbr.equals(xPlayerReference) || diagonal_trbl.equals(xPlayerReference)) {
+            gameComplete = true;
+            System.out.println("The winner is PLAYER X!");
+            System.exit(0);
+        } else if (row_0.equals(oPlayerReference) || row_1.equals(oPlayerReference) || row_2.equals(oPlayerReference) ||
+                column_0.equals(oPlayerReference) || column_1.equals(oPlayerReference) || column_2.equals(oPlayerReference) ||
+                diagonal_tlbr.equals(oPlayerReference) || diagonal_trbl.equals(oPlayerReference)) {
+            gameComplete = true;
+            System.out.println("The winner is PLAYER O!");
+            System.exit(0);
+        }
+    }
+
     public static void turnOfGame() {
         Character playerIcon = 'O';
         if (roundCounter % 2 == 0) {
             playerIcon = 'X';
         }
-        System.out.println("Counter: " + roundCounter);
+        if (roundCounter == 9) {
+            System.out.println("Game over. No Winner.");
+            System.exit(0);
+        }
+        //System.out.println("Counter: " + roundCounter);
         System.out.println("Player " + playerIcon + " select a spot on the grid");
         boolean selectedUserInput = userInput(playerIcon);
         while (!selectedUserInput) {
@@ -117,15 +153,10 @@ public class Main {
                     return false;
                 }
 
-
-                if (playArea.get(selected_y_cord-1).get(selected_x_cord-1) == null) {
+                // Check if empty non-played spot
+                if (playArea.get(selected_y_cord-1).get(selected_x_cord-1) == emptySlot) {
                     // Selected slot allowed.
-                    //System.out.println("POS_0: " + playArea.get(selected_y_cord-1).get(selected_x_cord-1));
-                    System.out.println("POS_0: " + playArea.get(selected_y_cord-1));
-                    // TODO: MAKE SURE THE CORRECT POSITION IS UPDATED!
-                    playArea.get(selected_y_cord-1).get(selected_x_cord-1);
-                    //System.out.println("POS_1: " + playArea.get(selected_y_cord-1).get(selected_x_cord-1));
-                    System.out.println("POS_1: " + playArea.get(selected_y_cord-1));
+                    playArea.get(selected_y_cord-1).set(selected_x_cord-1, playerIcon);
                     updateRowsColsAndDiags();
                     return true;
                 } else {
